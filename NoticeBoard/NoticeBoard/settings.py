d650fb1c30c197d3b3a9ecf13f2ece223f964a06
jagatkhatri01 <jagatkhatri383@gmail.com>
2024-01-12 17:27:42 +0545

    connected mysql

--- NoticeBoard/NoticeBoard/settings.py
+++ NoticeBoard/NoticeBoard/settings.py
@@ -74,12 +74,28 @@ WSGI_APPLICATION = 'NoticeBoard.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+import json
+
+with open('config.json') as config_file:
+    config = json.load(config_file)
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'testDB',
+        'USER': config['DB_USER'],
+        'PASSWORD': config['DB_PASSWORD'],
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
+
 
 
 # Password validation
