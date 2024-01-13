495184e644382f270bf90e9a6d1fcfec89ddd2bf
jagatkhatri01 <jagatkhatri383@gmail.com>
2024-01-13 18:15:04 +0545

    added pagination and search

--- NoticeBoard/notices/views.py
+++ NoticeBoard/notices/views.py
@@ -1,11 +1,31 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from django.http import HttpResponse
 from .models import NoticeBoard
+from django.db.models import Q
 
 # Create your views here.
 def notices(request):
+    query = request.GET.get('search')
     notices = NoticeBoard.objects.order_by('-datetime')
-    context = {'notices':notices, 'title':'College', 'site_name':'Notices'}
+
+    if query:
+        # Filter notices based on search query
+        notices = notices.filter(
+            Q(title__icontains=query) |
+            Q(content__icontains=query) |
+            Q(tags__icontains=query)
+        )
+
+    paginator = Paginator(notices, 5)
+    page_num = request.GET.get("page")
+    page_obj = paginator.get_page(page_num)
+    context = {'notices':notices,
+                'title':'College',
+                'site_name':'Notices',
+                'page_obj':page_obj,
+                'search_query':query
+                }
     return render(request, 'home.html', context)
 
 def home(request):
