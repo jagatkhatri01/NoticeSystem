a247a65fac09882c67bc3e299bd9e723312249c6
jagatkhatri01 <119671651+jagatkhatri01@users.noreply.github.com>
2024-04-06 21:22:22 +0545

    Update settings.py

--- NoticeBoard/NoticeBoard/settings.py
+++ NoticeBoard/NoticeBoard/settings.py
@@ -137,8 +137,11 @@ EMAIL_FILE_PATH = BASE_DIR / "sent_emails"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
 
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "ui/static")]
+STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "ui/staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
