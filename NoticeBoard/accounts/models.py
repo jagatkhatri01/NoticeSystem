4a9ebc66b8432ce9aa8f66b38667d9fdd3b0e7a1
jagatkhatri01 <jagatkhatri383@gmail.com>
2024-04-06 23:40:19 +0545

    success

--- NoticeBoard/accounts/models.py
+++ NoticeBoard/accounts/models.py
@@ -4,7 +4,6 @@ from django.db import models
 
 class CustomUser(AbstractUser):
     email = models.EmailField(unique=True)
-    # forget_password_token = models.CharField(max_length=100)
     role = models.CharField(max_length=10, choices=[('member', 'Member'), ('CR', 'Class Representative'), ('dean', 'Dean')], default='member')
-    department = models.CharField(max_length=50)
+    department = models.CharField(max_length=50, default=1)
     semester = models.IntegerField(default=1)
